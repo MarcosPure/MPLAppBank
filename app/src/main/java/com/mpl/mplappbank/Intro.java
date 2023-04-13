0b107c5f103041061be432dd8d1060da8bf2dbe1
MarcosPure <marcosmanyear@gmail.com>
2023-04-13 10:02:22 -0500

    guardado a las 10 am

--- app/src/main/java/com/mpl/mplappbank/Intro.java
+++ app/src/main/java/com/mpl/mplappbank/Intro.java
@@ -2,13 +2,33 @@ package com.mpl.mplappbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intro extends AppCompatActivity {
+    Button btnLogin, btnSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnLogin.setOnClickListener(this::Ir_Login);
+        btnSignUp.setOnClickListener(this::Ir_SignUp);
+
+    }
+
+    public void Ir_Login(View view) {
+        Intent intent=new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void Ir_SignUp(View view) {
+        Intent intent=new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
